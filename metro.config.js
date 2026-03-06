7a99f3d5f658556a54079166aceca64efc35510c
aaswani-v <avr9766@gmail.com>
2026-03-06 20:40:41 +0530

    fix(web): Use conditionNames to prefer CJS over ESM for all packages

--- metro.config.js
+++ metro.config.js
@@ -4,6 +4,12 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-config.resolver.sourceExts = config.resolver.sourceExts.filter((ext) => ext !== 'mjs');
+// Force Metro to prefer CJS builds over ESM (.mjs) to avoid
+// 'import.meta' errors from packages like zustand v5 on web.
+config.resolver.unstable_conditionNames = [
+  'react-native',
+  'require',
+  'default',
+];
 
 module.exports = config;
